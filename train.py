f9840d79952c8dc4d881b77ad9d4d8e00d90d3c4
Alireza Mostafizi <mostafia@pelican03.eecs.oregonstate.edu>
2019-03-15 16:11:42 -0700

    overwrite Dataset class to load the data

--- train.py
+++ train.py
@@ -31,6 +31,32 @@ def read_samples(csv_filepath, validation_per = 0.2):
     return training_samples, validation_samples 
 
 
+
+class Dataset(data.Dataset):
+    def __init__(self, samples, transform=None):
+        self.samples = samples
+        self.transform = transform
+    
+    def __getitem__(self, index):
+        batch_samples = self.samples[index)
+        steering_angle = float(batch_samples[3])
+        #center_img, steering_angle_center = augment(batch_samples[0], steering_angle)
+        #left_img, steering_angle_left = augment(batch_samples[1], steering_angle + 0.4)
+        #right_img, steering_angle_right = augment(batch_samples[2], steering_angle - 0.4)
+        center_img, steering_angle_center = batch_samples[0], steering_angle
+        left_img, steering_angle_left = batch_samples[1], steering_angle + 0.4
+        right_img, steering_angle_right = batch_samples[2], steering_angle - 0.4
+        center_img = self.transform(center_img)
+        left_img = self.transform(left_img)
+        right_img = self.transform(right_img)
+        return (center_img, steering_angle_center),\
+               (left_img, steering_angle_left),\
+               (right_img, steering_angle_right)
+               
+    def __len__(self):
+        return len(self.samples)
+
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
